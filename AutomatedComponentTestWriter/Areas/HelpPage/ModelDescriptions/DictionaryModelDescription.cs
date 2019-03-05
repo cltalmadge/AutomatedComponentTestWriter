e105719114f326122154f105bde56d4c9b4d545a
Caelan L. Talmadge <talmadge12@gmail.com>
2019-03-05 10:38:12 -0600

    Add project files.

---
+++ AutomatedComponentTestWriter/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AutomatedComponentTestWriter.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AutomatedComponentTestWriter/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AutomatedComponentTestWriter.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
