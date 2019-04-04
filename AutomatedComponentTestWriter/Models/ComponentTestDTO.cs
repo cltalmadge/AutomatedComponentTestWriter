e44fb59a5067bc6711f8d9dfa0b9d4f544b4af7a
Caelan L. Talmadge <talmadge12@gmail.com>
2019-04-04 09:25:02 -0500

    Added some objects to handle complex types.

--- AutomatedComponentTestWriter/Models/ComponentTestDTO.cs
+++ AutomatedComponentTestWriter/Models/ComponentTestDTO.cs
@@ -5,8 +5,6 @@ using System.Web;
 
 namespace AutomatedComponentTestWriter.Models
 {
-
-
     public class ComponentTestDTO
     {
         public ComponentTestDTO()
@@ -21,26 +19,46 @@ namespace AutomatedComponentTestWriter.Models
 
     public class Property
     {
-        public Property()
-        {
-            Parameters = new List<Parameter>();
-        }
         public string PropertyName { get; set; }
         public string DataType { get; set; }
         public string Required { get; set; }
         public string DefaultValue { get; set; }
+        public ComplexObject ComplexType { get; set; }
         public List<Parameter> Parameters { get; set; }
+
+        public Property()
+        {
+            Parameters = new List<Parameter>();
+        }
     }
 
     public class Parameter
     {
         public string ExpectedMessage { get; set; }
         public string HTTPResponse { get; set; }
-        public string RandomParam { get; set; }
-        public string NullParam { get; set; }
-        public string BlankParam { get; set; }
+        public string RandomParam { get; set; } = "False";
+        public string NullParam { get; set; } = "False";
+        public string BlankParam { get; set; } = "False";
         public string ValueLength { get; set; }
         public string TestName { get; set; }
+    }
+
+    public class ComplexObject
+    {
+        public string ObjectName { get; set; }
+        public List<ComplexObjectMember> ComplexMembers { get; set; }
+
+        public ComplexObject()
+        {
+            ComplexMembers = new List<ComplexObjectMember>();
+        }
+    }
+
+    public class ComplexObjectMember
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string DataType { get; set; }
     }
 
     public enum APIAction
